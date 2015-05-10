f049e004600c0790bfd4ba8ef7dc9a6bef73ff96
Jan Duffhauss <jan.duffhauss@chess-ix.com>
2015-05-10 23:47:55 +0200

    Join result files into one total.csv

--- src/main/java/BankReader/BatchConfiguration.java
+++ src/main/java/BankReader/BatchConfiguration.java
@@ -4,6 +4,7 @@ package BankReader
 import BankReader.file.ABN.ABNBankLine;
 import BankReader.file.ABN.ABNProcessor;
 import BankReader.file.GenericBankLine;
+import BankReader.file.GenericProcessor;
 import BankReader.file.ING.INGBankLine;
 import BankReader.file.ING.INGProcessor;
 import org.springframework.batch.core.Job;
@@ -16,8 +17,11 @@ import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.MultiResourceItemReader;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.mapping.FieldSetMapper;
+import org.springframework.batch.item.file.mapping.PassThroughFieldSetMapper;
 import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
 import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
@@ -26,6 +30,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import sun.net.www.content.text.Generic;
+
+import java.io.File;
+import java.io.IOException;
 
 @Configuration
 @EnableBatchProcessing
@@ -37,10 +47,25 @@ public class BatchConfiguration {
     @Autowired
     private StepBuilderFactory steps;
 
+    @Autowired
+    private ResourcePatternResolver resourcePatternResolver;
+
     @Value("${input.directory}")
     protected String inputDirectory;
 
 
+    // Job definition
+    @Bean
+    public Job getJob() {
+        return jobs.get("bankProcessingJob")
+                .start(abnStep())
+                .next(ingStep())
+                .next(resultFilesStep())
+                .build();
+    }
+
+
+    // Bank ABN
     @Bean
     public ItemReader<ABNBankLine> abnReader() {
 
@@ -48,7 +73,7 @@ public class BatchConfiguration {
         reader.setResource(new FileSystemResource(inputDirectory + "/abn.csv"));
         reader.setLineMapper(new DefaultLineMapper<ABNBankLine>() {{
             setLineTokenizer(new DelimitedLineTokenizer() {{
-                setNames(new String[] {"Rekeningnummer", "Muntsoort", "Transactiedatum", "Rentedatum", "Beginsaldo", "Eindsaldo", "Transactiebedrag", "Omschrijving" });
+                setNames(new String[]{"Rekeningnummer", "Muntsoort", "Transactiedatum", "Rentedatum", "Beginsaldo", "Eindsaldo", "Transactiebedrag", "Omschrijving"});
             }});
             setFieldSetMapper(new BeanWrapperFieldSetMapper<ABNBankLine>() {{
                 setTargetType(ABNBankLine.class);
@@ -63,7 +88,7 @@ public class BatchConfiguration {
     	FlatFileItemWriter<GenericBankLine> writer = new FlatFileItemWriter<GenericBankLine>();
         writer.setResource(new FileSystemResource(inputDirectory + "/abn-output.csv"));
     	DelimitedLineAggregator<GenericBankLine> delLineAgg = new DelimitedLineAggregator<GenericBankLine>();
-    	delLineAgg.setDelimiter(",");
+    	delLineAgg.setDelimiter("###");
     	BeanWrapperFieldExtractor<GenericBankLine> fieldExtractor = new BeanWrapperFieldExtractor<GenericBankLine>();
     	fieldExtractor.setNames(new String[] {"category", "subCategory", "date", "amount", "description"});
     	delLineAgg.setFieldExtractor(fieldExtractor);
@@ -86,15 +111,8 @@ public class BatchConfiguration {
                 .build();
     }
 
-    @Bean
-    public Job getJob() {
-        return jobs.get("bankProcessingJob")
-                .start(abnStep())
-                .next(ingStep())
-                .build();
-    }
 
-
+    // Bank ING
 
     @Bean
     public ItemReader<INGBankLine> ingReader() {
@@ -118,7 +136,7 @@ public class BatchConfiguration {
         FlatFileItemWriter<GenericBankLine> writer = new FlatFileItemWriter<GenericBankLine>();
         writer.setResource(new FileSystemResource(inputDirectory + "/ing-output.csv"));
         DelimitedLineAggregator<GenericBankLine> delLineAgg = new DelimitedLineAggregator<GenericBankLine>();
-        delLineAgg.setDelimiter(",");
+        delLineAgg.setDelimiter("###");
         BeanWrapperFieldExtractor<GenericBankLine> fieldExtractor = new BeanWrapperFieldExtractor<GenericBankLine>();
         fieldExtractor.setNames(new String[] {"category", "subCategory", "date", "amount", "description"});
         delLineAgg.setFieldExtractor(fieldExtractor);
@@ -138,6 +156,63 @@ public class BatchConfiguration {
                 .reader(ingReader())
                 .processor(ingProcessor())
                 .writer(ingWriter())
+                .build();
+    }
+
+
+    // Join result files
+    @Bean
+    public ItemReader<GenericBankLine> genericReader() {
+
+        FlatFileItemReader<GenericBankLine> reader = new FlatFileItemReader<GenericBankLine>();
+        reader.setLineMapper(new DefaultLineMapper<GenericBankLine>() {{
+            setLineTokenizer(new DelimitedLineTokenizer("###") {{
+                setNames(new String[]{"category", "subCategory", "date", "amount", "description"});
+            }});
+            setFieldSetMapper(new BeanWrapperFieldSetMapper<GenericBankLine>() {{
+                setTargetType(GenericBankLine.class);
+            }});
+        }});
+
+        MultiResourceItemReader multiResourceItemReader = new MultiResourceItemReader();
+        Resource[] resources;
+        try {
+            String pattern = "file:" + inputDirectory + "/*output.csv";
+            resources = resourcePatternResolver.getResources(pattern);
+        } catch (IOException e) {
+            throw new RuntimeException("I/O problems when resolving the input file pattern.",e);
+        }
+        multiResourceItemReader.setResources(resources);
+        multiResourceItemReader.setDelegate(reader);
+
+        return multiResourceItemReader;
+    }
+
+    @Bean
+    public ItemProcessor<GenericBankLine, GenericBankLine> genericProcessor() {
+        return new GenericProcessor();
+    }
+
+    @Bean
+    public ItemWriter<GenericBankLine> genericWriter() {
+        FlatFileItemWriter<GenericBankLine> writer = new FlatFileItemWriter<GenericBankLine>();
+        writer.setResource(new FileSystemResource(inputDirectory + "/total.csv"));
+        DelimitedLineAggregator<GenericBankLine> delLineAgg = new DelimitedLineAggregator<GenericBankLine>();
+        delLineAgg.setDelimiter("###");
+        BeanWrapperFieldExtractor<GenericBankLine> fieldExtractor = new BeanWrapperFieldExtractor<GenericBankLine>();
+        fieldExtractor.setNames(new String[] {"category", "subCategory", "date", "amount", "description"});
+        delLineAgg.setFieldExtractor(fieldExtractor);
+        writer.setLineAggregator(delLineAgg);
+        return writer;
+    }
+
+    @Bean
+    public Step resultFilesStep() {
+        return steps.get("joinResultFilesStep")
+                .<GenericBankLine, GenericBankLine> chunk(5)
+                .reader(genericReader())
+                .processor(genericProcessor())
+                .writer(genericWriter())
                 .build();
     }
 
